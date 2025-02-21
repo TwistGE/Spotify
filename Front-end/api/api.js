5cc57dfd49e7537b67fbfdcbf508e31ce77a2ff7
TwistGE <otaviotm09@gmail.com>
2025-02-21 13:53:44 -0300

    Deploy Error2

--- Front-end/api/api.js
+++ Front-end/api/api.js
@@ -1,11 +1,11 @@
-import "dotenv/config"
+import "dotenv/config";
 import axios from "axios";
 
 const {NODE_ENV} = process.env;
 const URL = NODE_ENV === "development" ? "http://localhost:3002/api" : "/api";
 
-const responseArtists = await axios.get(`${URL}/artists`)
-const responseSongs = await axios.get(`${URL}/songs`)
+const responseArtists = await axios.get(`${URL}/artists`);
+const responseSongs = await axios.get(`${URL}/songs`);
 
 export const artistArray = responseArtists.data;
 export const songsArray = responseSongs.data;
