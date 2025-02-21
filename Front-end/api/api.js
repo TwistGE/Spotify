ffe8c37c38db49bdf04750ea77c0bd7900f4229f
TwistGE <otaviotm09@gmail.com>
2025-02-21 14:13:51 -0300

    Deploy process Error

--- Front-end/api/api.js
+++ Front-end/api/api.js
@@ -1,8 +1,8 @@
-import "dotenv/config";
+
 import axios from "axios";
 
-const {NODE_ENV} = process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3002/api" : "/api";
+// const {NODE_ENV} = process.env;
+const URL = "https://spotify-b7u9.onrender.com/api";
 
 const responseArtists = await axios.get(`${URL}/artists`);
 const responseSongs = await axios.get(`${URL}/songs`);
